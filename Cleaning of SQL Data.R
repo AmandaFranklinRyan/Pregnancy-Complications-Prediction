d299fad3cbb04f89bdfc4db4ddc45c3faadb184a
Amanda Franklin-Ryan <amanda.franklin.ryan@gmail.com>
2023-05-21 15:09:57 +0200

    First working version with pregnancy data

--- Cleaning of SQL Data.R
+++ Cleaning of SQL Data.R
@@ -1,25 +1,30 @@
 library(tidyverse)
 
-#Import datasets
+#Import medical notes to extract maternal data
 
 discharge_notes <- read.csv("Data/DISCHARGE_SUMMARIES_ORIG.csv")
 
 discharge_notes_cleaned <- discharge_notes %>% 
   select(-DESCRIPTION,-CATEGORY)
 
-example <- "This is not very complicated This is not"
-str_extract(example,"([^\\s]+\\s+){2}complicated(\\s+[^\\s]+){2}")
+
+# Extract key maternal information from infants' medical notes ------------
+
+# Extract mother's age using regular expressions
+#From looking at notes need to incorporate newline characters into the Regex
 
 notes_age <- discharge_notes_cleaned %>% 
-  mutate(age=str_extract(TEXT,"\\d+[\\n -]*year[\\n -]*old")) %>% 
-  filter(is.na(age))
-
-"\\d+[\\n -]*year[\\n -]*old"
-"\\d+[ -]year-old"
+  mutate(age=str_extract(TEXT,"\\d+[\\n -]*year[\\n -]*old"))
 
 
+#Check percent of missing data
+# Find ages for 92% of women, quick check of other records shows age data is apparently missing
 age_sum <- sum(is.na(notes_age$age))# total number of NAs in the complicated column
 age_fraction <- (age_sum/nrow(notes_age))
+
+# Extract number fo pregnancies and parity 
+notes_pregnancy <- notes_age %>% 
+  mutate(pregnancy=str_extract(TEXT,"(?i)Gravida\\s*([IVX]+|\\d+)[,]?\\s*Para\\s*([IVX]+|\\d+)"))
 
 notes_complicated <- discharge_notes_cleaned %>% 
   mutate(clean_text=str_replace_all(TEXT, "[[:punct:]]", "")) %>% 
