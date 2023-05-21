abe4a9c5333ff60902b329e25233f3a8c0fd75ba
Amanda Franklin-Ryan <amanda.franklin.ryan@gmail.com>
2023-05-21 14:46:40 +0200

    Add age data for mothers from notes

--- Cleaning of SQL Data.R
+++ Cleaning of SQL Data.R
@@ -11,8 +11,15 @@ example <- "This is not very complicated This is not"
 str_extract(example,"([^\\s]+\\s+){2}complicated(\\s+[^\\s]+){2}")
 
 notes_age <- discharge_notes_cleaned %>% 
-  mutate(age=str_extract(TEXT,"\\d+[ -]year-old")) 
+  mutate(age=str_extract(TEXT,"\\d+[\\n -]*year[\\n -]*old")) %>% 
+  filter(is.na(age))
 
+"\\d+[\\n -]*year[\\n -]*old"
+"\\d+[ -]year-old"
+
+
+age_sum <- sum(is.na(notes_age$age))# total number of NAs in the complicated column
+age_fraction <- (age_sum/nrow(notes_age))
 
 notes_complicated <- discharge_notes_cleaned %>% 
   mutate(clean_text=str_replace_all(TEXT, "[[:punct:]]", "")) %>% 
